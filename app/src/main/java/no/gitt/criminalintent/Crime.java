dcba183d53df74937d69bbcfedffa896d1be1ead
Gard Marius Guldhav <gard@gitt.no>
2016-12-29 18:48:18 +0100

    Crime model and fragmentactivity

---
+++ app/src/main/java/no/gitt/criminalintent/Crime.java
@@ -0,0 +1,29 @@
+package no.gitt.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by gard on 29.12.16.
+ */
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+}
